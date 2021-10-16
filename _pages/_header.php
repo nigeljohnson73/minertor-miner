5ed4a8ef691305d6a696a61675c1e52bae14997d
Nigel Johnson <nigel@nigeljohnson.net>
2021-10-16 20:34:35 +0100

    removed coinbase warning

--- _pages/_header.php
+++ _pages/_header.php
@@ -47,9 +47,3 @@ startPage();
 		</div>
 		<div class="container-lg">
 			<!-- Ended in the footer -->
-			<?php
-			global $coinbase_privKey;
-			if (strlen($coinbase_privKey) == 0) {
-				echo "<div class='alert alert-danger'> No coinbase keys are setup in config_" . $_SERVER["SERVER_NAME"] . ".php</div>";
-			}
-			?>
