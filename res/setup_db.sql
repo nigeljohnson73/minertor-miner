7661677c30b88e7d4da088a4be0bbc56e4311b44
Nigel Johnson <nigel@nigeljohnson.net>
2021-10-17 00:03:12 +0100

    Correct database set up

--- res/setup_db.sql
+++ res/setup_db.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE cc;
+CREATE DATABASE mtm;
 CREATE USER 'mtm_user'@'localhost' IDENTIFIED BY 'mtm_passwd';
 GRANT ALL PRIVILEGES ON mtm.* TO 'mtm_user'@'localhost';
 #ALTER USER 'mtm_user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'mtm_passwd'; # Does not work on the pi - needed it for Mac development setup
