175ef836d9d36ec06ff182f95b94cd054c6bbab2
Nigel Johnson <nigel@nigeljohnson.net>
2021-10-17 10:01:00 +0100

    Added tabed browser for small devices

--- _pages/home.php
+++ _pages/home.php
@@ -68,69 +68,90 @@
 		</div> <!-- big panel -->
 
 		<div class="d-block d-sm-none panel-display">
-			<h1>Coming soon</h1>
-			<div class='data-container server-stats' data-ng-show='server'>
-				<div class='data-section server-stats'>
-					<div class='data-row'>
-						<div class='banner'>Server</div>
+			<ul class="nav nav-tabs" id="myTab" role="tablist">
+				<li class="nav-item" role="presentation">
+					<button class="nav-link active" id="server-tab" data-bs-toggle="tab" data-bs-target="#server" type="button" role="tab" aria-controls="server" aria-selected="true">Server</button>
+				</li>
+				<li class="nav-item" role="presentation">
+					<button class="nav-link" id="alarms-tab" data-bs-toggle="tab" data-bs-target="#alarms" type="button" role="tab" aria-controls="alarms" aria-selected="false">Alarms</button>
+				</li>
+				<li class="nav-item" role="presentation">
+					<button class="nav-link" id="miners-tab" data-bs-toggle="tab" data-bs-target="#miners" type="button" role="tab" aria-controls="miners" aria-selected="false">Miners</button>
+				</li>
+			</ul>
+			<div class="tab-content" id="myTabContent">
+				<div class="tab-pane fade show active" id="server" role="tabpanel" aria-labelledby="server-tab">
+					<div class='data-container server-stats' data-ng-show='server'>
+						<div class='data-section server-stats'>
+							<!-- <div class='data-row'>
+								<div class='banner'>Server</div>
+							</div> -->
+							<div class='data-row'>
+								<div class='label'>Temperature:</div>
+								<div class='value' highlight-on-change='{{server.temperature}}'>{{server.temperature | number:2}} {{server.temperature_unit}}</div>
+							</div>
+							<div class='data-row'>
+								<div class='label'>CPU load:</div>
+								<div class='value' highlight-on-change='{{server.cpu_load}}'>{{server.cpu_load | number:2}}%</div>
+							</div>
+							<div class='data-row'>
+								<div class='label'>RAM load:</div>
+								<div class='value' highlight-on-change='{{server.mem_load}}'>{{server.mem_load | number:2}}%</div>
+							</div>
+							<div class='data-row'>
+								<div class='label'>HDD load:</div>
+								<div class='value' highlight-on-change='{{server.cpu_load}}'>{{server.sd_load | number:2}}%</div>
+							</div>
+							<div class='data-row'>
+								<div class='label'>CPU Frequency:</div>
+								<div class='value' highlight-on-change='{{server.frequency_cpu}}'>{{server.frequency_cpu | number:0}} {{server.frequency_unit}}</div>
+							</div>
+							<div class='data-row'>
+								<div class='label'>VPN:</div>
+								<div class='value' highlight-on-change='{{server.vpn_connection}}'>{{server.vpn_connection}}</div>
+							</div>
+						</div>
 					</div>
-					<div class='data-row'>
-						<div class='label'>Temperature:</div>
-						<div class='value' highlight-on-change='{{server.temperature}}'>{{server.temperature | number:2}} {{server.temperature_unit}}</div>
+				</div>
+				<div class="tab-pane fade" id="alarms" role="tabpanel" aria-labelledby="alarms-tab">
+					<div class='data-container server-alarms' data-ng-show='server'>
+						<div class='data-section'>
+							<!-- <div class='data-row'>
+								<div class='banner'>Alarms</div>
+							</div> -->
+							<div class='data-row'>
+								<div class='label'>VPN:</div>
+								<div class='value alarm alarm_{{server.vpn_alarm}}'>{{server.vpn_alarm}}</div>
+							</div>
+							<div class='data-row'>
+								<div class='label'>Throttled:</div>
+								<div class='value alarm alarm_{{server.throttled}}'>{{server.throttled}}</div>
+							</div>
+							<div class='data-row'>
+								<div class='label'>Capped:</div>
+								<div class='value alarm alarm_{{server.frequency_capped}}'>{{server.frequency_capped}}</div>
+							</div>
+							<div class='data-row'>
+								<div class='label'>Soft temp:</div>
+								<div class='value alarm alarm_{{server.soft_temperature_limited}}'>{{server.soft_temperature_limited}}</div>
+							</div>
+							<div class='data-row'>
+								<div class='label'>Under volt:</div>
+								<div class='value alarm alarm_{{server.under_voltage}}'>{{server.under_voltage}}</div>
+							</div>
+						</div>
 					</div>
-					<div class='data-row'>
-						<div class='label'>CPU load:</div>
-						<div class='value' highlight-on-change='{{server.cpu_load}}'>{{server.cpu_load | number:2}}%</div>
-					</div>
-					<div class='data-row'>
-						<div class='label'>RAM load:</div>
-						<div class='value' highlight-on-change='{{server.mem_load}}'>{{server.mem_load | number:2}}%</div>
-					</div>
-					<div class='data-row'>
-						<div class='label'>HDD load:</div>
-						<div class='value' highlight-on-change='{{server.cpu_load}}'>{{server.sd_load | number:2}}%</div>
-					</div>
-					<div class='data-row'>
-						<div class='label'>CPU Frequency:</div>
-						<div class='value' highlight-on-change='{{server.frequency_cpu}}'>{{server.frequency_cpu | number:0}} {{server.frequency_unit}}</div>
-					</div>
-					<div class='data-row'>
-						<div class='label'>VPN:</div>
-						<div class='value' highlight-on-change='{{server.vpn_connection}}'>{{server.vpn_connection}}</div>
-					</div>
+				</div>
+				<div class="tab-pane fade" id="miners" role="tabpanel" aria-labelledby="miners-tab">
+					<h1>Coming soon</h1>
+					<button type="button" class="btn btn-custom" onclick="location.reload()">Reload</button>
 				</div>
 			</div>
 
-			<div class='data-container server-alarms' data-ng-show='server'>
-				<div class='data-section'>
-					<div class='data-row'>
-						<div class='banner'>Alarms</div>
-					</div>
-					<div class='data-row'>
-						<div class='label'>VPN:</div>
-						<div class='value alarm alarm_{{server.vpn_alarm}}'>{{server.vpn_alarm}}</div>
-					</div>
-					<div class='data-row'>
-						<div class='label'>Throttled:</div>
-						<div class='value alarm alarm_{{server.throttled}}'>{{server.throttled}}</div>
-					</div>
-					<div class='data-row'>
-						<div class='label'>Capped:</div>
-						<div class='value alarm alarm_{{server.frequency_capped}}'>{{server.frequency_capped}}</div>
-					</div>
-					<div class='data-row'>
-						<div class='label'>Soft temp:</div>
-						<div class='value alarm alarm_{{server.soft_temperature_limited}}'>{{server.soft_temperature_limited}}</div>
-					</div>
-					<div class='data-row'>
-						<div class='label'>Under volt:</div>
-						<div class='value alarm alarm_{{server.under_voltage}}'>{{server.under_voltage}}</div>
-					</div>
-				</div>
-			</div>
-			<p>Small panel filler.</p>
-			<p>Small panel filler.</p>
-			<button type="button" class="btn btn-custom" onclick="location.reload()">Reload</button>
+
+
+
+
 		</div> <!-- small panel -->
 	</div> <!-- loading -->
 </div> <!-- container -->
